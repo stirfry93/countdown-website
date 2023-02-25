76942dd8986fdae2a077a878f38d62c545fed332
Sterling <sterlingmrichards@gmail.com>
2023-02-25 14:44:12 -0600

    Add countdown js

---
+++ script.js
@@ -0,0 +1,28 @@
+// First set the date I'm counting down to, which is April 15th, 2023
+let countDownDate = new Date("Apr 15, 2023 00:00:00").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+    // Get today's date and time
+    let now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    let distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (100 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EDPIRED";
+    }
+}, 1000);
