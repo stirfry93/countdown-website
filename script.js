8e5ad78c81810595c167b2a15acd3bbdf2128b91
Sterling <sterlingmrichards@gmail.com>
2023-02-25 15:15:06 -0600

    change countdown expiration message

--- script.js
+++ script.js
@@ -23,6 +23,6 @@ let x = setInterval(function() {
     // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EDPIRED";
+        document.getElementById("demo").innerHTML = "MORE TIME TOGETHER STARTING NOW!";
     }
 }, 1000);
